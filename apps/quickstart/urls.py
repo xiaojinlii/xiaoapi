9a65c1340902202ca100c4e51bd0613687bc9526
JinLiGame <553555614@qq.com>
2024-03-12 21:30:15 +0800

    自定义响应类

--- apps/quickstart/urls.py
+++ apps/quickstart/urls.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
-
+from core.response import SuccessResponse
 
 app = APIRouter()
 
 
 @app.get("/test", summary="测试接口")
 def test():
-    return JSONResponse(content="hello world!")
+    return SuccessResponse(data="hello world!")
