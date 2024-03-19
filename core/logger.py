83069683a7fb89274d7af33fde4582ea7b6e166f
JinLiGame <553555614@qq.com>
2024-03-19 15:52:58 +0800

    日志保留时间修改为30天

--- core/logger.py
+++ core/logger.py
@@ -19,5 +19,5 @@ if not os.path.exists(log_path):
 log_path_info = os.path.join(log_path, f'info_{time.strftime("%Y-%m-%d")}.log')
 log_path_error = os.path.join(log_path, f'error_{time.strftime("%Y-%m-%d")}.log')
 
-info = logger.add(log_path_info, rotation="00:00", retention="3 days", enqueue=True, encoding="UTF-8", level="INFO")
-error = logger.add(log_path_error, rotation="00:00", retention="3 days", enqueue=True, encoding="UTF-8", level="ERROR")
+info = logger.add(log_path_info, rotation="00:00", retention="30 days", enqueue=True, encoding="UTF-8", level="INFO")
+error = logger.add(log_path_error, rotation="00:00", retention="30 days", enqueue=True, encoding="UTF-8", level="ERROR")
