47895b73a8704c9bbf7de3b295165faaa2e91f3b
JinLiGame <553555614@qq.com>
2024-03-26 14:48:26 +0800

    Update migrate.py

--- fastapi_xiaojinli/management/commands/migrate.py
+++ fastapi_xiaojinli/management/commands/migrate.py
@@ -2,16 +2,14 @@ import asyncio
 
 from typer import Typer
 
-from fastapi_xiaojinli.db.migrate import create_tables as migrate_create_tables
-
 
 def register_command(shell: Typer):
-
     @shell.command()
     def create_tables():
         """
         初始化数据库
         需要迁移到数据库的模型 要在application/configs/database_config.py的MIGRATE_MODELS里配置
         """
+        from fastapi_xiaojinli.db.migrate import create_tables as migrate_create_tables
         asyncio.run(migrate_create_tables())
         print("数据库表结构创建成功！")
