636537ca5b920360a18b13a3a9bba72b30e4e6c9
JinLiGame <553555614@qq.com>
2024-03-26 19:21:09 +0800

    修改命令管理器

--- fastapi_xiaojinli/management/__init__.py
+++ fastapi_xiaojinli/management/__init__.py
@@ -2,11 +2,11 @@ import typer
 
 from fastapi_xiaojinli.management.commands import register_manage_commands, register_admin_commands
 
-manage_commands = typer.Typer()
+manage_commands = typer.Typer(no_args_is_help=True, add_completion=False)
 register_manage_commands(manage_commands)
 
 
-admin_commands = typer.Typer()
+admin_commands = typer.Typer(no_args_is_help=True, add_completion=False)
 register_admin_commands(admin_commands)
 
 manage_commands.add_typer(admin_commands, name="admin", help="admin命令")
