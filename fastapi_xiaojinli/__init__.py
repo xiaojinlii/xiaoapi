5f2434ed87526126ae9dbe050ac60053de65ae8c
JinLiGame <553555614@qq.com>
2024-03-26 20:29:33 +0800

    修改包名

--- fastapi_xiaojinli/__init__.py
+++ fastapi_xiaojinli/__init__.py
@@ -1,10 +1,1 @@
-
 __version__ = "0.0.1"
-
-# todo logger 会import settings，先注释
-# from .core.logger import logger
-
-
-__all__ = [
-    # "logger",
-]
--- fastapi_xiaojinli/core/__init__.py
+++ fastapi_xiaojinli/core/__init__.py
@@ -1,3 +1,4 @@
 from .exception import CustomException
+from .fastapi import get_fastapi_application
 from .logger import logger
 from .utils import import_modules, import_functions, import_functions_async
