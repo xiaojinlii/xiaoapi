c7450134c401e9414350f63404522ba5c5669707
小锦鲤 <553555614@qq.com>
2024-03-28 11:12:25 +0800

    固定xiaoapi版本号

--- libs/sqlalchemy/xiaoapi_sqlalchemy/__init__.py
+++ libs/sqlalchemy/xiaoapi_sqlalchemy/__init__.py
@@ -2,6 +2,9 @@ __version__ = "0.0.4"
 
 
 from .crud import DalBase
-# from .database import async_engine, session_factory, Base, db_getter
 from .dependencies import QueryParams, Paging, IdList
+
+# 由于 database 中使用 settings.SQLALCHEMY_DATABASE_URL，所以在这里导入容易引发异常： "Requested settings, but settings are not configured.
+# You must define the environment variable XIAOAPI_SETTINGS_MODULE before accessing settings."
+# from .database import async_engine, session_factory, Base, db_getter
 # from .model_base import BaseModel
